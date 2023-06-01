838d790d2455ef07b7bc6b5107490efe8e7115c1
Kwadwo Sarbeng-Baafi <35800995+Sarbeng@users.noreply.github.com>
2023-06-01 17:25:39 +0000

    error status begun

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,33 +1,32 @@
-
-interface Props{
+interface Props {
   name?: string;
   type?: string;
-  label?:string;
-  value?:string;
-  ref?:string;
-  handleChange: (e?: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => void;
+  label?: string;
+  value?: string;
+  ref?: string;
+  handleChange: (
+    e?:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => void;
   handleBlur: React.FocusEventHandler<HTMLInputElement> | undefined;
 }
 
-export default function TextInput  (props:Props) {
-    return (
-        
-            <div id="inputGroup">
-              <label className="" htmlFor="email">
-                {props.label}
-              </label>
-              <input
-                className="mt-2 mb-2 h-12 border border-primary-border rounded-lg w-full py-2 px-3 text-primary-main focus:outline-none  focus:border-primary-focused focus:ring-1 shadow-sm focus:ring-primary-focused "
-                name={props.name}
-                type={props.type}
-                value={props.value}
-                ref={props.ref}
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-
-              />
-              
-            </div>
-       
-    );
+export default function TextInput(props: Props) {
+  return (
+    <div id="inputGroup">
+      <label className="" htmlFor="email">
+        {props.label}
+      </label>
+      <input
+        className="mt-2 mb-2 h-12 border border-primary-border rounded-lg w-full py-2 px-3 text-primary-main focus:outline-none  focus:border-primary-focused focus:ring-1 shadow-sm focus:ring-primary-focused "
+        name={props.name}
+        type={props.type}
+        value={props.value}
+        ref={props.ref}
+        onChange={props.handleChange}
+        onBlur={props.handleBlur}
+      />
+    </div>
+  );
 }
