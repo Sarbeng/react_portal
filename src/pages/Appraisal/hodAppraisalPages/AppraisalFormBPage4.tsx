dfc89618e4e55b5f80986df132d67c2aa7d073cd
Sarbeng <sarbengbaafi@gmail.com>
2023-07-31 11:52:13 +0000

    interpersonal communication page

--- src/pages/Appraisal/hodAppraisalPages/AppraisalFormBPage4.tsx
+++ src/pages/Appraisal/hodAppraisalPages/AppraisalFormBPage4.tsx
@@ -46,67 +46,67 @@ export default function AppraisalFormBPage4() {
                                 <label className="capitalize" htmlFor="email">
                                 Relates well to all people – up, down, and across – internally and externally to the School/Department
                                 </label>
-                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.aspiring_rank && formik.errors.aspiring_rank ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
-                                name="aspiring_rank"
+                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.relates_well && formik.errors.relates_well ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
+                                name="relates_well"
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
-                                value={formik.values.aspiring_rank}>
+                                value={formik.values.relates_well}>
                                     <option selected disabled>Select Rank</option>
-                                    <option>Administrator</option>
-                                    <option>Administrator 2</option>
+                                    <option>Satisfactory</option>
+                                    <option>Unsatisfactory</option>
                                 </select>
-                                {formik.touched.aspiring_rank && formik.errors.aspiring_rank ? (
-                        <div className="text-red-600">{formik.errors.aspiring_rank}</div>
+                                {formik.touched.relates_well && formik.errors.relates_well ? (
+                        <div className="text-red-600">{formik.errors.relates_well}</div>
                       ) : null}
                             </div>
                             <div id="inputGroup">
                                 <label className="capitalize" htmlFor="email">
                                 Establishes rapport; builds and maintains effective working relationships
                                 </label>
-                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.aspiring_rank && formik.errors.aspiring_rank ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
-                                name="aspiring_rank"
+                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.establishes_rapport && formik.errors.establishes_rapport ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
+                                name="establishes_rapport"
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
-                                value={formik.values.aspiring_rank}>
+                                value={formik.values.establishes_rapport}>
                                     <option selected disabled>Select Rank</option>
-                                    <option>Administrator</option>
-                                    <option>Administrator 2</option>
+                                    <option>Successful</option>
+                                    <option>Unsuccessful</option>
                                 </select>
-                                {formik.touched.aspiring_rank && formik.errors.aspiring_rank ? (
-                        <div className="text-red-600">{formik.errors.aspiring_rank}</div>
+                                {formik.touched.establishes_rapport && formik.errors.establishes_rapport ? (
+                        <div className="text-red-600">{formik.errors.establishes_rapport}</div>
                       ) : null}
                             </div> <div id="inputGroup">
                                 <label className="capitalize" htmlFor="email">
                                 Practices attentive and active listening
                                 </label>
-                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.aspiring_rank && formik.errors.aspiring_rank ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
-                                name="aspiring_rank"
+                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.practices_active_listening && formik.errors.practices_active_listening ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
+                                name="practices_active_listening"
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
-                                value={formik.values.aspiring_rank}>
+                                value={formik.values.practices_active_listening}>
                                     <option selected disabled>Select Rank</option>
-                                    <option>Administrator</option>
-                                    <option>Administrator 2</option>
+                                    <option>Successful</option>
+                                    <option>Unsuccessful</option>
                                 </select>
-                                {formik.touched.aspiring_rank && formik.errors.aspiring_rank ? (
-                        <div className="text-red-600">{formik.errors.aspiring_rank}</div>
+                                {formik.touched.practices_active_listening && formik.errors.practices_active_listening ? (
+                        <div className="text-red-600">{formik.errors.practices_active_listening}</div>
                       ) : null}
                             </div>
                              <div id="inputGroup">
                                 <label className="capitalize" htmlFor="email">
                                 Uses diplomacy and tact; can diffuse high-tension situations comfortably
                                 </label>
-                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.aspiring_rank && formik.errors.aspiring_rank ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
-                                name="aspiring_rank"
+                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.uses_diplomacy && formik.errors.uses_diplomacy ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`} 
+                                name="uses_diplomacy"
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
-                                value={formik.values.aspiring_rank}>
+                                value={formik.values.uses_diplomacy}>
                                     <option selected disabled>Select Rank</option>
-                                    <option>Administrator</option>
-                                    <option>Administrator 2</option>
+                                    <option>Above Expectations</option>
+                                    <option>Below Expectations</option>
                                 </select>
-                                {formik.touched.aspiring_rank && formik.errors.aspiring_rank ? (
-                        <div className="text-red-600">{formik.errors.aspiring_rank}</div>
+                                {formik.touched.uses_diplomacy && formik.errors.uses_diplomacy ? (
+                        <div className="text-red-600">{formik.errors.uses_diplomacy}</div>
                       ) : null}
                             </div>
                             
